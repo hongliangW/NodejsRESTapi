59b1aeca1c712fbd6bdc9afa5aed17d972d8cc31
Hongliang Wang <hongliang7876@gmail.com>
2018-03-03 14:22:43 -0500

    Created basic CRUD and added middleware to GET method

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import routes from './src/routes/restRoutes';
 
 const app = express();
 const PORT = 8000;
+
+routes(app);
 
 app.get('/', (req, res) => res.send(`Node and Express server is running on port ${PORT}`));
 
