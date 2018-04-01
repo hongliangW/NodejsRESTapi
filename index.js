69cb8bf318a1a68c0754b0aab662887026bdc38e
Hongliang Wang <hongliang7876@gmail.com>
2018-04-01 19:18:13 -0400

    Set up database

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser'; //allowing to send an object or data through HTTP post
 import routes from './src/routes/restRoutes';
 
 const app = express();
 const PORT = 8000;
+
+//mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/CRMdb', {
+  useMongoClient: true
+});
+
+//set up bodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 routes(app);
 
